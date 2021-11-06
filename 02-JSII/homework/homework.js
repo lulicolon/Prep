0482cbcb6ecf1b19dece387a9535884d1f94e3ea
Luján Colón <wwwlu_000@Scoonflix.(none)>
2021-11-06 09:51:59 -0300

    tarea finalizada

--- 02-JSII/homework/homework.js
+++ 02-JSII/homework/homework.js
@@ -3,21 +3,25 @@
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
-
+  return [array.length - 1];
 }
 
 function incrementarPorUno(array) {
   // "array" debe ser una arreglo de números enteros
   // Aumenta cada número por 1 y devuelve el array
   // Tu código:
-
+  for (var i = 0; i < array.length; i++) {
+    array[i] = array[i] + 1;
+  } 
+  return array;
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-
+  array.push(elemento);
+  return array;
 }
 
 function dePalabrasAFrase(palabras) {
@@ -26,21 +30,30 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-
+  var frase = palabras[0];
+  for (let i = 1; i < palabras.length; i++) {
+    frase = frase + " " + palabras[i]; 
+  }
+  return frase;
 }
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === elemento) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser un array de enteros
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-
+  
 }
 
 function numeroMasGrande(numeros) {
